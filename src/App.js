0c550bc45737054103483831b64c6b3c0b781e37
Ljubiša Moćić <ljubisa.mocic@codecentric.de>
2019-05-23 14:59:27 +0200

    Remove AsyncStorage

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, { useEffect, useState } from 'react';
-
-import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState } from 'react';
 
 import AppContainer from './Navigation';
 import UserContext from './context/UserContext';
-import { TOKEN_KEY } from './Constants';
 
 const App = () => {
   const [username, setUsername] = useState('');
   const [token, setToken] = useState('');
 
-  useEffect(() => {
-    // TODO
-    loadToken();
-  }, []);
-
-  const loadToken = async () => {
-    try {
-      const storedToken = await AsyncStorage.getItem(TOKEN_KEY);
-      if (storedToken !== undefined && storedToken !== '') {
-        setToken(storedToken);
-      }
-    } catch (e) {
-      console.error('Failed to get TOKEN');
-    }
-  };
-
   const loginUser = async (newUsername, newToken) => {
     setUsername(newUsername);
     setToken(newToken);
-    try {
-      await AsyncStorage.setItem(TOKEN_KEY, newToken);
-    } catch (e) {
-      console.error('Failed to save TOKEN to AsyncStorage');
-    }
   };
 
   const logoutUser = () => {
