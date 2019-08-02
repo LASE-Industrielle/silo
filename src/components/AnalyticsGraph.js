b67f241c81e4079e717b611d68778459b7b7750c
milosobradovic <milos.obradovic@codecentric.de>
2019-08-02 10:42:21 +0200

    Implement analytics graph with victory-native

---
+++ src/components/AnalyticsGraph.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { VictoryAxis, VictoryChart, VictoryLine, VictoryTheme, VictoryLabel, LineSegment } from 'victory-native';
+
+const AnalyticsGraph = props => {
+  const { ticks } = props;
+  let { data } = props;
+  const [loader, setLoader] = useState(false);
+
+  if (data.length === 1) {
+    // fix bug in charts when only one value needs to be shown
+    data = [...data, { x: 1, y: data[0].y }];
+  }
+
+  // hack chart drawing to be done in background
+  useEffect(() => {
+    (async () => {
+      setLoader(true);
+    })();
+  }, []);
+
+  return (
+    <View>
+      {data && loader ? (
+        <VictoryChart
+          domainPadding={{x: [5, 5], y: [0, 30]}}
+          padding={{ left: 55, right: 50, bottom: 40, top: 20 }}
+          height={props.height}
+          theme={VictoryTheme.material}
+          style={{parent: {backgroundColor: '#6CC799'}}}
+        >
+          <VictoryAxis
+            dependentAxis
+            orientation="left"
+            style={{
+              axis: { stroke: 'transparent' },
+              tickLabels: {stroke: 'white', fontWeight: 'bold'},
+              ticks: { stroke: 'transparent' },
+              grid: { stroke: '#5EA890', strokeDasharray: '3,3' }
+            }}
+            tickLabelComponent={<VictoryLabel dy={-10} dx={10} style={{fill: 'white', fontSize: 12}}/>}
+          />
+
+          <VictoryAxis
+            orientation="bottom"
+            style={{
+              axis: { stroke: '#4E9685', strokeWidth: 3, width: 100 },
+              ticks: { stroke: 'transparent' },
+              grid: { stroke: '#5EA890', strokeDasharray: '3,3' }
+            }}
+            axisComponent={<LineSegment x2={310}/>}
+            tickLabelComponent={<VictoryLabel text={(datum) => [data[datum - 1].x.split(' ')[0], data[datum - 1].x.split(' ')[1]]}
+                                              style={{fill: 'white', fontSize: 12}}/>}
+          />
+
+          <VictoryLine style={{ data: { stroke: 'white' } }} data={data} interpolation="catmullRom" />
+        </VictoryChart>
+      ) : (
+        <View style={{ height: 200, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator animating />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default AnalyticsGraph;
--- src/screens/AnalyticsScreen.js
+++ src/screens/AnalyticsScreen.js
@@ -1,14 +1,12 @@
-import React, {useEffect} from 'react';
-import {ActivityIndicator, Dimensions, RefreshControl, ScrollView, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, RefreshControl, ScrollView, View} from 'react-native';
 import {Body, Button, Container, Header, Icon, Left, List, ListItem, Right, Text, Title,} from 'native-base';
 
 import styles from '../Styles';
 
-import {LineChart} from 'react-native-chart-kit';
-
-import {primary, secondary} from '../Colors';
 import getAllMeasurements from '../services/MeasurementService';
 import {useStateValue} from "../context/StateContext";
+import AnalyticsGraph from '../components/AnalyticsGraph';
 
 
 const AnalyticsScreen = (props) => {
@@ -16,42 +14,33 @@ const AnalyticsScreen = (props) => {
   const [{measurements}, dispatch] = useStateValue();
   const siloId = props.navigation.getParam('id', 1);
 
+  const getDataMeasurements = () => {
+    let measurementsArray = Object.keys(measurements.data);
+    let dataMeasurementsArray = [];
+    for(let i = 0; i <= measurementsArray.length; i++) {
+      if(i === 7){
+        break;
+      }
+      let dataMeasurement = {};
+      dataMeasurement.x = new Date(measurementsArray[i]).toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric'
+      });
+
+      let yValues = Object.values(measurements.data[measurementsArray[i]]);
+      dataMeasurement.y = yValues[0];
+      dataMeasurementsArray.push(dataMeasurement);
+    }
+    console.log(dataMeasurementsArray.reverse());
+    return dataMeasurementsArray;
+  };
+
   useEffect(() => {
     getAllMeasurements(dispatch, siloId);
   }, []);
 
   const onRefresh = () => {
     getAllMeasurements(dispatch, siloId);
-  };
-
-  const getLabels = () => {
-    const labels = [];
-
-    Object.keys(measurements.data)
-      .map(key => {
-        labels.push(new Date(key).toLocaleDateString('en-US', {
-          month: 'short',
-          day: 'numeric'
-        }));
-      });
-
-    return labels.reverse();
-  };
-
-  const getAveragePercentages = () => {
-    const array = [];
-
-    Object.keys(measurements.data)
-      .map(key => {
-        let average = 0;
-        Object.keys(measurements.data[key])
-          .map(percentage => {
-            average += measurements.data[key][percentage];
-          });
-        average = average / Object.keys(measurements.data[key]).length;
-        array.push(average);
-      });
-    return array.reverse();
   };
 
   const analyticsList = () => {
@@ -96,39 +85,14 @@ const AnalyticsScreen = (props) => {
     {(Object.entries(measurements.data).length === 0 && measurements.data.constructor )? <ActivityIndicator
       animating={true}
       size="large"
-  />:( <View  style={{marginBottom: 0, height: 220}}>
-    <LineChart
-        data={{
-          labels: getLabels(),
-          datasets: [{
-            data: getAveragePercentages()
-          }]
-        }}
-        width={Math.round(Dimensions.get('window').width)} // from react-native
-        height={220}
-        chartConfig={{
-          backgroundColor: primary,
-          backgroundGradientFrom: primary,
-          backgroundGradientTo: secondary,
-          decimalPlaces: 1, // optional, defaults to 2dp
-          color: () => `rgba(255, 255, 255, 0.9)`,
-          style: {
-            borderRadius: 12,
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-          }
-        }}
-        bezier
-        style={{marginBottom: 0}}
-    />
-
-  </View>)}
+  />:
+      ( <View style={{marginBottom: 0, height: 220}}>
+        <AnalyticsGraph
+          loading={false}
+          data={getDataMeasurements()}
+          height={220}
+        />
+      </View>)}
     <ScrollView style={{position: 'relative', width: '100%', marginTop:0}} refreshControl={<RefreshControl
         refreshing={measurements.loading}
         onRefresh={onRefresh}
